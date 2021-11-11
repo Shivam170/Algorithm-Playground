5ee6b2dc0124e8ece0370850f3645d3891623b6b
Shivam170 <shivamsharma4723@gmail.com>
2021-11-11 22:04:51 +0530

    Sorting

--- src/AlgoPlayground/GFG/Sorting/PartitionArray.java
+++ src/AlgoPlayground/GFG/Sorting/PartitionArray.java
@@ -20,4 +20,22 @@ public class PartitionArray {
             arr[i] = temp[i-l];
         }
     }
+    void swap(int[] arr,int i, int j){
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+    
+    int lomutoPartition(int[] arr,int l, int h){
+        int pivot = arr[h];
+        int i = l-1;
+        for (int j = 0; j <= h-1; j++) {
+            if (arr[j]<pivot){
+                i++;
+                swap(arr,i,j);
+            }
+        }
+        swap(arr,i+1,h);
+        return i+1;
+    }
 }
