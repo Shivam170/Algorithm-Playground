b6d17d594345dfdce0ab30c5faac70592ea75cbb
Shivam170 <shivamsharma4723@gmail.com>
2021-11-11 11:23:06 +0530

    Sorting

--- src/AlgoPlayground/GFG/Sorting/MergeFunction.java
+++ src/AlgoPlayground/GFG/Sorting/MergeFunction.java
@@ -1,7 +1,38 @@
 package AlgoPlayground.GFG.Sorting;
 
 public class MergeFunction {
-    void merge(){
-        
+    void merge(int[] a,int low,int mid,int high){
+        int n1 = mid-low+1;
+        int n2 = high-mid;
+        int[] left = new int[n1];
+        int[] right = new int[n2];
+        for (int i = 0; i < n1 ; i++) {
+            left[i] = a[low+i];
+        }
+        for (int i = 0; i < n2; i++) {
+            right[i] = a[n1+i];
+        }
+        int i=0,j=0,k=0;
+        while(i<n1 && j<n2){
+            if(left[i]<=right[j]){
+                a[k]=left[i];
+                i++;
+                k++;
+            }else{
+                a[k] = right[j];
+                j++;
+                k++;
+            }
+        }
+        while(i<n1){
+            a[k]=left[i];
+            i++;
+            k++;
+        }
+        while(j<n2){
+            a[k]=right[j];
+            j++;
+            k++;
+        }
     }
 }
