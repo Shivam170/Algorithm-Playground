2c423b07ea3864d6f3554d221e98104267fef2b6
Shivam170 <shivamsharma4723@gmail.com>
2021-11-12 15:53:28 +0530

    Sorting

--- src/AlgoPlayground/GFG/Sorting/QuickSort.java
+++ src/AlgoPlayground/GFG/Sorting/QuickSort.java
@@ -1,5 +1,29 @@
 package AlgoPlayground.GFG.Sorting;
 
 public class QuickSort {
-    void quickSort(){}
+    void quickSort(int[] arr, int l, int h){
+        if(l<h){
+            int p = lomutoPartition(arr,l,h);
+            quickSort(arr,l,p-1);
+            quickSort(arr,p+1,h);
+        }
+    }
+    void swap(int[] arr,int i, int j){
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    int lomutoPartition(int[] arr,int l, int h){
+        int pivot = arr[h];
+        int i = l-1;
+        for (int j = 0; j <= h-1; j++) {
+            if (arr[j]<pivot){
+                i++;
+                swap(arr,i,j);
+            }
+        }
+        swap(arr,i+1,h);
+        return i+1;
+    }
 }
