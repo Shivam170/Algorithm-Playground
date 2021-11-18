c00c3191a33d9427615918fa40bbf11742a7161a
Shivam170 <shivamsharma4723@gmail.com>
2021-11-18 21:54:36 +0530

    Sorting

--- src/AlgoPlayground/GFG/Array/SecondLargest.java
+++ src/AlgoPlayground/GFG/Array/SecondLargest.java
@@ -1,11 +1,34 @@
 package AlgoPlayground.GFG.Array;
 
 public class SecondLargest {
-    int secondLargest(int arr[]){
+    int secondLargest(int arr[], int n){
         int largest = LargestElement.getLargest(arr);
         int res = -1;
-        //for
-        return 0;
+        for (int i = 0; i < n ; i++) {
+            if (arr[i]!=arr[largest]){
+                if (res==-1){
+                    res=i;
+                }else if (arr[i]>arr[res]){
+                    res = i;
+                }
+            }
+        }
+        return res;
+    }
+
+    int secondLargestEff(int[] arr, int n){
+        int res = -1, largest = 0;
+        for (int i = 1; i < n; i++) {
+            if (arr[i]>arr[largest]){
+                res = largest;
+                largest = i;
+            }else if(arr[i]!=arr[largest]){
+                if (res == -1 || arr[i] > arr[res]){
+                    res = i;
+                }
+            }
+        }
+        return res;
     }
 
 }
