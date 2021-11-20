84b9f269a9474fcf04ffbf6cc5cbad67a21d8310
Shivam170 <shivamsharma4723@gmail.com>
2021-11-20 08:37:56 +0530

    Searching

--- src/AlgoPlayground/GFG/Searching/BinarySearch.java
+++ src/AlgoPlayground/GFG/Searching/BinarySearch.java
@@ -13,4 +13,12 @@ public class BinarySearch {
         }
         return -1;
     }
+
+    int recursiveBinarySearch(int[] arr, int low, int high, int x){
+        if (low>high) return -1;
+        int mid = (low+high)/2;
+        if (arr[mid]==x) return mid;
+        else if (arr[mid] > x) return recursiveBinarySearch(arr,low,mid-1,x);
+        else return recursiveBinarySearch(arr,mid+1,high,x);
+    }
 }
