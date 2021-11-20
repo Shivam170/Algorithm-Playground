c13b4d1e2ef4f56e124405ac7b188c515f6d5561
Shivam170 <shivamsharma4723@gmail.com>
2021-11-20 08:55:10 +0530

    Searching

--- src/AlgoPlayground/GFG/Searching/FirstOccurence.java
+++ src/AlgoPlayground/GFG/Searching/FirstOccurence.java
@@ -5,6 +5,7 @@ public class FirstOccurence {
 
     // Efficient Solution (Binary Search) : O(log(n)) Time complexity and O(1) Space Complexity
 
+    // Recursive Solution
     int firstOcc(int[] arr, int low, int high, int x){
         if (low > high) return -1;
         int mid = (low + high)/2;
@@ -17,5 +18,23 @@ public class FirstOccurence {
                 return mid;
             }else return firstOcc(arr,low,mid-1,x);
         }
+    }
+
+    // Iterative Solution
+    int firstOccIter(int[] arr, int n, int x){
+        int low = 0, high = n-1;
+        while(low<=high){
+            int mid = (low+high)/2;
+            if (arr[mid]>x){
+                high = mid -1;
+            }else if (arr[mid]<x){
+                low = mid +1;
+            }else {
+                if (mid==0 || arr[mid-1]!=arr[mid]){
+                    return mid;
+                } else high = mid-1;
+            }
+        }
+        return -1;
     }
 }
