6c94a8a4a15fcee555d5060fda1c371c942d7fd8
Shivam170 <shivamsharma4723@gmail.com>
2022-01-07 23:06:08 +0530

    Day 7

--- src/AlgoPlayground/GFG/Hashing/MoreThanN_KOcc.java
+++ src/AlgoPlayground/GFG/Hashing/MoreThanN_KOcc.java
@@ -1,24 +1,39 @@
 package AlgoPlayground.GFG.Hashing;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MoreThanN_KOcc {
     public static void main(String[] args) {
 
     }
-    void printN_K(int[] arr, int n, int k){
+
+    void printN_K(int[] arr, int n, int k) {
         Arrays.sort(arr);
         int i = 1, count = 1;
-        while(i<n){
-            while(i<n && arr[i]==arr[i-1]){
+        while (i < n) {
+            while (i < n && arr[i] == arr[i - 1]) {
                 count++;
                 i++;
             }
-            if (count>n/k){
-                System.out.println(arr[i-1]+" ");
+            if (count > n / k) {
+                System.out.println(arr[i - 1] + " ");
             }
-            count=1;
+            count = 1;
             i++;
+        }
+    }
+
+    void printN_KEff(int[] arr, int n, int k) {
+        HashMap<Integer, Integer> hm = new HashMap<>();
+        for (int x : arr) {
+            hm.put(x, hm.getOrDefault(x,0)+1);
+        }
+        for (Map.Entry<Integer,Integer> e : hm.entrySet()){
+            if (e.getValue()>n/k){
+                System.out.println(e.getKey()+" ");
+            }
         }
     }
 }
