8d4a97c09f2d86a1bd1ea3088f97f6898508c441
Shivam170 <shivamsharma4723@gmail.com>
2022-01-09 20:52:04 +0530

    Day 9

--- src/AlgoPlayground/GFG/String/NaivePattern.java
+++ src/AlgoPlayground/GFG/String/NaivePattern.java
@@ -14,4 +14,23 @@ public class NaivePattern {
                 System.out.print(i+" ");
         }
     }
+
+    void improvedNaive(String str, String pat){
+        int n = str.length();
+        int m = pat.length();
+        for (int i = 0; i <= n-m;) {
+            int j;
+            for (j=0;j<m;j++){
+                if (pat.charAt(j)!=str.charAt(i+j)){
+                    break;
+                }
+            }
+            if (j==m){
+                System.out.print(i+" ");
+            }
+            if (j==0){
+                i++;
+            }else i=i+j;
+        }
+    }
 }
