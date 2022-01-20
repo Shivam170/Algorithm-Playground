cbaf94a68fc2644e79a5a135b287e3f2a707ddb7
Shivam170 <shivamsharma4723@gmail.com>
2022-01-21 00:25:11 +0530

    Day 21

--- src/AlgoPlayground/GFG/Graph/BFS.java
+++ src/AlgoPlayground/GFG/Graph/BFS.java
@@ -1,10 +1,12 @@
 package AlgoPlayground.GFG.Graph;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Queue;
 
 public class BFS {
+    // Connected Graph and source is given
     void BFS(ArrayList<ArrayList<Integer>> adj, int V, int s) {
         boolean[] visited = new boolean[V + 1];
         Queue<Integer> q = new LinkedList<Integer>();
@@ -18,6 +20,31 @@ public class BFS {
                     visited[v] = true;
                     q.add(v);
                 }
+            }
+        }
+    }
+
+    void BFSHelper(ArrayList<ArrayList<Integer>> adj, int s, boolean[] visited) {
+        Queue<Integer> q = new LinkedList<>();
+        visited[s] = true;
+        q.add(s);
+        while (!q.isEmpty()) {
+            int u = q.poll();
+            System.out.print(u + " ");
+            for (int v : adj.get(u)) {
+                if (!visited[v]) {
+                    visited[v] = true;
+                    q.add(v);
+                }
+            }
+        }
+    }
+
+    void BFSDisconnected(ArrayList<ArrayList<Integer>> adj, int V) {
+        boolean[] visited = new boolean[V + 1];
+        for (int i = 0; i < V; i++) {
+            if (!visited[i]) {
+                BFSHelper(adj, i, visited);
             }
         }
     }
