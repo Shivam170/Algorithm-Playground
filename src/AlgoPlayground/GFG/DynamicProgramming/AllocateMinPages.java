31e0a4221c207a78e4aa499019f66cd67ddb943f
Shivam170 <shivamsharma4723@gmail.com>
2022-01-31 11:27:03 +0530

    Day 31

--- src/AlgoPlayground/GFG/DynamicProgramming/AllocateMinPages.java
+++ src/AlgoPlayground/GFG/DynamicProgramming/AllocateMinPages.java
@@ -1,6 +1,26 @@
 package AlgoPlayground.GFG.DynamicProgramming;
 
 public class AllocateMinPages {
+    int minPages(int[] arr, int n, int k) {
+        int[][] dp = new int[k + 1][n + 1];
+        for (int i = 1; i <= n; i++) {
+            dp[1][i] = sum(arr, 0, i - 1);
+        }
+        for (int i = 1; i <= k; i++) {
+            dp[i][1] = arr[0];
+        }
+        for (int i = 2; i <= k; i++) {
+            for (int j = 2; j <= n; j++) {
+                int res = Integer.MAX_VALUE;
+                for (int p = 1; p < j; p++) {
+                    res = Math.min(res, Math.max(dp[i - 1][p], sum(arr, p, j - 1)));
+                }
+                dp[i][j] = res;
+            }
+        }
+        return dp[k][n];
+    }
+
     int sum(int[] arr, int b, int e) {
         int s = 0;
         for (int i = b; i <= e; i++) {
