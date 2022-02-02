da322aa46cfa1e3ef31fbb3a00a9192d111dab7c
Shivam170 <shivamsharma4723@gmail.com>
2022-02-02 19:37:17 +0530

    2 Feb

--- src/AlgoPlayground/GFG/LinkedList/ReverseDLL.java
+++ src/AlgoPlayground/GFG/LinkedList/ReverseDLL.java
@@ -1,7 +1,16 @@
 package AlgoPlayground.GFG.LinkedList;
 
 public class ReverseDLL {
-    static DNode reverse(DNode head){
-
+    static DNode reverse(DNode head) {
+        if (head == null || head.next == null) return head;
+        DNode temp = head;
+        DNode prev = null;
+        while (temp != null) {
+            prev = temp.prev;
+            temp.prev = temp.next;
+            temp.next = prev;
+            temp = temp.prev;
+        }
+        return prev.prev;
     }
 }
