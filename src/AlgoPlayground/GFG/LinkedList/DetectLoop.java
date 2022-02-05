d8d784439a9d9e8acfa0df4465a4f17b1a1cc4c3
Shivam170 <shivamsharma4723@gmail.com>
2022-02-05 21:46:33 +0530

    5 Feb

--- src/AlgoPlayground/GFG/LinkedList/DetectLoop.java
+++ src/AlgoPlayground/GFG/LinkedList/DetectLoop.java
@@ -1,5 +1,7 @@
 package AlgoPlayground.GFG.LinkedList;
 
+import java.util.HashSet;
+
 public class DetectLoop {
     // TC - O(n^2)
     // For every node check if next is already seen before
@@ -19,6 +21,33 @@ public class DetectLoop {
             curr.next = temp;
             curr = currNext;
         }
+        return false;
+    }
 
+    // This method uses Hashing
+    boolean detectLoopHashing(Node head) {
+        HashSet<Node> set = new HashSet<>();
+        Node temp = head;
+        while (temp != null) {
+            if (set.contains(temp)) {
+                return true;
+            }
+            set.add(temp);
+            temp = temp.next;
+        }
+        return false;
+    }
+
+    // Using Floyd cycle detection method
+    boolean detectLoop(Node head) {
+        Node slow = head, fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                return true;
+            }
+        }
+        return false;
     }
 }
