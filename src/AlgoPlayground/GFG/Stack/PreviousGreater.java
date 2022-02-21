569031ee47f2de845bef5396dd8d19d3382b4a06
Shivam170 <shivamsharma4723@gmail.com>
2022-02-21 11:31:39 +0530

    Feb 21

---
+++ src/AlgoPlayground/GFG/Stack/PreviousGreater.java
@@ -0,0 +1,31 @@
+package AlgoPlayground.GFG.Stack;
+
+import java.util.ArrayDeque;
+
+public class PreviousGreater {
+    void naive(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            int j;
+            for (j = i - 1; j >= 0; j--) {
+                if (arr[j] > arr[i]) {
+                    System.out.print(arr[j] + " ");
+                    break;
+                }
+            }
+            if (j == -1) System.out.print(-1 + " ");
+        }
+    }
+
+    void prevGreater(int[] arr) {
+        ArrayDeque<Integer> s = new ArrayDeque<>();
+        s.push(arr[0]);
+        for (int i = 0; i < arr.length; i++) {
+            while (!s.isEmpty() && s.peek() <= arr[i]) {
+                s.pop();
+            }
+            int pg = s.isEmpty() ? -1 : s.peek();
+            System.out.print(pg);
+            s.push(arr[i]);
+        }
+    }
+}
