9314dcb1e1a66b5e8d7ae440a8a0331c6ad214ae
Shivam170 <shivamsharma4723@gmail.com>
2022-02-22 14:16:55 +0530

    Feb 22

--- src/AlgoPlayground/misc/Tree/BinaryTree/Diameter.java
+++ src/AlgoPlayground/misc/Tree/BinaryTree/Diameter.java
@@ -9,14 +9,25 @@ public class Diameter {
         int rightHeight = height(root.right);
         return Math.max(leftHeight, rightHeight) + 1;
     }
-    int diameter(TreeNode root){
-        if (root == null){
+
+    int diameter(TreeNode root) {
+        if (root == null) {
             return 0;
         }
         int lHeight = height(root.left);
         int rHeight = height(root.right);
         int lDiameter = diameter(root.left);
         int rDiameter = diameter(root.right);
-        return Math.max(lHeight+rHeight+1,Math.max(lDiameter,rDiameter));
+        return Math.max(lHeight + rHeight + 1, Math.max(lDiameter, rDiameter));
+    }
+
+    int res = 0;
+
+    int heightD(TreeNode root) {
+        if (root == null) return 0;
+        int lh = heightD(root.left);
+        int rh = heightD(root.right);
+        res = Math.max(res, 1 + lh + rh);
+        return 1 + Math.max(lh, rh);
     }
 }
--- src/AlgoPlayground/misc/Tree/BinaryTree/Practice.java
+++ src/AlgoPlayground/misc/Tree/BinaryTree/Practice.java
@@ -1,19 +1,7 @@
 package AlgoPlayground.misc.Tree.BinaryTree;
 
-
-import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Stack;
-
 public class Practice {
-   static int diameter = Integer.MIN_VALUE;
-    static int diameter(TreeNode root){
-        if(root==null) {
-            return 0;
-        }
-        return 1;
-    }
+
     public static void main(String[] args) {
         BinaryTree bt = new BinaryTree();
         TreeNode head = null;
