45e251055d07a13a64717b66a7f5cba9775ed1e2
Shivam170 <shivamsharma4723@gmail.com>
2022-02-22 13:04:07 +0530

    Feb 22

--- src/AlgoPlayground/misc/Tree/BinaryTree/CheckBalance.java
+++ src/AlgoPlayground/misc/Tree/BinaryTree/CheckBalance.java
@@ -7,8 +7,19 @@ public class CheckBalance {
         }
         int leftHeight = height(root.left);
         int rightHeight = height(root.right);
-        if(Math.abs(leftHeight-rightHeight)>=1) this.isBalanced = false;
+        if (Math.abs(leftHeight - rightHeight) >= 1) this.isBalanced = false;
         return Math.max(leftHeight, rightHeight) + 1;
     }
+
     boolean isBalanced = true;
+
+    public int isBalanced(TreeNode root) {
+        if (root == null) return 0;
+        int lh = isBalanced(root.left);
+        if (lh == -1) return -1;
+        int rh = isBalanced(root.right);
+        if (rh == -1) return -1;
+        if (Math.abs(lh - rh) > 1) return -1;
+        else return Math.max(lh, rh) + 1;
+    }
 }
