2d417c3636a5a80bac71774114839f4ab3875428
Shivam170 <shivamsharma4723@gmail.com>
2022-02-23 09:44:15 +0530

    Feb 23

--- src/AlgoPlayground/GFG/Tree/BinarySearchTree/SearchBST.java
+++ src/AlgoPlayground/GFG/Tree/BinarySearchTree/SearchBST.java
@@ -1,5 +1,27 @@
 package AlgoPlayground.GFG.Tree.BinarySearchTree;
 
 public class SearchBST {
-    boolean search( root)
+    //Recursive
+    boolean search(TreeNode root, int x) {
+        if (root == null) {
+            return false;
+        } else if (x == root.data) {
+            return true;
+        } else if (root.data < x) {
+            return search(root.right, x);
+        } else {
+            return search(root.left, x);
+        }
+    }
+
+    boolean searchIterative(TreeNode root, int x) {
+        while (root != null) {
+            if (root.data == x) {
+                return true;
+            } else if (root.data < x) {
+                root = root.right;
+            } else root = root.left;
+        }
+        return false;
+    }
 }
---
+++ src/AlgoPlayground/GFG/Tree/BinarySearchTree/TreeNode.java
@@ -0,0 +1,21 @@
+package AlgoPlayground.GFG.Tree.BinarySearchTree;
+
+public class TreeNode {
+    TreeNode left, right;
+    int data;
+    TreeNode(){
+
+    }
+    TreeNode(int data) {
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+
+    TreeNode(int data, TreeNode left, TreeNode right) {
+        this.data = data;
+        this.left = left;
+        this.right = right;
+    }
+
+}
