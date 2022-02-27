95cbb82960f96591f0d20e08a2aa4df657e7883d
Shivam170 <shivamsharma4723@gmail.com>
2022-02-27 20:16:01 +0530

    Feb 27

--- src/AlgoPlayground/GFG/Heap/KClosest.java
+++ src/AlgoPlayground/GFG/Heap/KClosest.java
@@ -1,6 +1,40 @@
 package AlgoPlayground.GFG.Heap;
 
+import java.util.PriorityQueue;
+
+class Pair implements Comparable<Pair> {
+    int diff;
+    int idx;
+
+    Pair(int d, int i) {
+        diff = d;
+        idx = i;
+    }
+
+    @Override
+    public int compareTo(Pair pair) {
+        return pair.diff - this.diff;
+    }
+}
+
 public class KClosest {
+    void kClosest(int[] arr, int k, int x, int n) {
+        PriorityQueue<Pair> pq = new PriorityQueue<>();
+        for (int i = 0; i < k; i++) {
+            pq.add(new Pair(arr[i] - x, i));
+        }
+        for (int i = k; i < n; i++) {
+            int diff = Math.abs(arr[i] - x);
+            if (pq.peek().diff > diff) {
+                pq.poll();
+                pq.add(new Pair(diff, i));
+            }
+        }
+        for (int i = 0; i < k; i++) {
+            System.out.print(arr[pq.poll().idx] + " ");
+        }
+    }
+
     void naive(int[] arr, int k, int x, int n) {
         boolean[] visited = new boolean[n];
         for (int i = 0; i < k; i++) {
