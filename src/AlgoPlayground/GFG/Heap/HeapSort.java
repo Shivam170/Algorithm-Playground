0b5d29015eee545c5b73948a3f39090e20857f50
Shivam170 <shivamsharma4723@gmail.com>
2022-02-27 12:07:47 +0530

    Feb 27

--- src/AlgoPlayground/GFG/Heap/HeapSort.java
+++ src/AlgoPlayground/GFG/Heap/HeapSort.java
@@ -1,4 +1,25 @@
 package AlgoPlayground.GFG.Heap;
 
 public class HeapSort {
+    void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    void maxHeapify(int[] arr, int n, int i) {
+        int largest = i, left = 2 * i + 1, right = 2 * i + 2;
+        if (left < n && arr[left] > arr[largest]) largest = left;
+        if (right < n && arr[right] > arr[largest]) largest = right;
+        if (largest != i) {
+            swap(arr, largest, i);
+            maxHeapify(arr, n, largest);
+        }
+    }
+
+    void buildHeap(int[] arr, int n) {
+        for (int i = (n - 2) / 2; i >= 0; i--) {
+            maxHeapify(arr, n, i);
+        }
+    }
 }
