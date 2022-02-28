55b55054c6443d5f36d53d0c0a7c1677f1cfa2ee
Shivam170 <shivamsharma4723@gmail.com>
2022-02-28 11:12:18 +0530

    Feb 28

--- src/AlgoPlayground/GFG/Trie/TrieNode.java
+++ src/AlgoPlayground/GFG/Trie/TrieNode.java
@@ -4,3 +4,31 @@ public class TrieNode {
     TrieNode[] child = new TrieNode[26];
     boolean isEnd;
 }
+
+class Operation {
+    TrieNode root;
+
+    void insert(String key) {
+        TrieNode curr = root;
+        for (int i = 0; i < key.length(); i++) {
+            int index = key.charAt(i) - 'a';
+            if (curr.child[index] == null) {
+                curr.child[index] = new TrieNode();
+            }
+            curr = curr.child[index];
+        }
+        curr.isEnd = true;
+    }
+
+    boolean search(String key) {
+        TrieNode curr = root;
+        for (int i = 0; i < key.length(); i++) {
+            int index = key.charAt(i) - 'a';
+            if (curr.child[index] == null) {
+                return false;
+            }
+            curr = curr.child[index];
+        }
+        return curr.isEnd;
+    }
+}
