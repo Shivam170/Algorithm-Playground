d0713182a2dae7dabdd1783614a0c34e094808db
Shivam170 <shivamsharma4723@gmail.com>
2022-02-28 10:32:51 +0530

    Feb 28

--- src/AlgoPlayground/GFG/Matrix/Median.java
+++ src/AlgoPlayground/GFG/Matrix/Median.java
@@ -1,4 +1,25 @@
 package AlgoPlayground.GFG.Matrix;
 
+import java.util.Arrays;
+
 public class Median {
+    int matMed(int[][] mat, int r, int c) {
+        int min = mat[0][0], max = mat[0][c - 1];
+        for (int i = 1; i < r; i++) {
+            if (mat[i][0] < min) min = mat[i][0];
+            if (mat[i][c - 1] > max) max = mat[i][c - 1];
+        }
+        int medPos = (r * c + 1) / 2;
+        while (min < max) {
+            int mid = (min + max) / 2, midPos = 0;
+            for (int i = 0; i < r; i++) {
+                int pos = Arrays.binarySearch(mat[i], mid) + 1;
+                midPos += Math.abs(pos);
+            }
+            if (midPos < medPos)
+                min = mid + 1;
+            else max = mid;
+        }
+        return min;
+    }
 }
